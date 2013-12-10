c2b6b5b56827b0282cf7e4a99c2c208fb1653e17
sushmitharaos <sushmitharaos@gmail.com>
2013-12-10 15:10:38 +0530

    Shruthi, Sush | #900 | adding more migrations

--- src/main/resources/V1_12__Registration.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO person_attribute_type (name, description, format, searchable, creator, date_created, retired, sort_weight, uuid) VALUES ('class', 'Class', 'java.lang.String', '1', 1, curdate(), 0, 2, uuid());
