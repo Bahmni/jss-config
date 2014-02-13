bf886b53452c537f58eff2c7fffa875accd333d2
Vinay Venu <vinayv@thoughtworks.com>
2014-02-13 14:54:17 +0530

    Get rid of functions and loops

--- release-scripts/3-0/saleOrderMigration.sql
+++ release-scripts/3-0/saleOrderMigration.sql
@@ -1,13 +1,13 @@
-drop table if exists sale_order_events_temp;
-
-select concat('{', 
+insert into event_records(create_uid, create_date, write_date, write_uid, category, uuid, 
+    title, timestamp, object, uri) select 1, now(), now(), 1, 'sale_order', 
+    uuid_generate_v4(), 'sale_order', now(), a.content, '' 
+from (select concat('{', 
     '"id":"', order_id, '",',
     '"order_date":"', order_date, '",',
     '"external_id":"', external_id, '",',
     '"customer_id":"', customer_id, '",',
     '"sale_order_items":', '[', string_agg(product_info, ','), ']',
     '}') as content
-into sale_order_events_temp
 from
     (
     select so.id as order_id, so.create_date as order_date, so.external_id as external_id, res_partner.ref as customer_id,
@@ -27,34 +27,4 @@ from
     join res_partner on res_partner.id = partner_id
     join product_uom pu on pu.id = s.product_uom 
     ) as foo
-group by order_id, order_date, external_id, customer_id;
-
-ALTER TABLE sale_order_events_temp ADD COLUMN id SERIAL;
-UPDATE sale_order_events_temp SET id = nextval(pg_get_serial_sequence('sale_order_events_temp','id'));
-
-CREATE OR REPLACE FUNCTION insert_sale_order_event_records()
-    RETURNS integer AS
-$BODY$
-DECLARE
-    sale_order_count integer;
-    sale_order_start_id integer;
-    title_value integer;
-    i integer;
-BEGIN
-    select id into sale_order_start_id from sale_order_events_temp order by id limit 1;
-    SELECT count(*)+sale_order_start_id into sale_order_count from sale_order_events_temp;
-    select title into title_value from event_records where category = 'sale_order';
-    i := id;
-    WHILE (i <= sale_order_count)
-    loop
-        title_value = title_value + 1;
-        insert into event_records values (
-            1, now(), now(), 1, 'sale_order', 
-            uuid_generate_v4(), title_value, now(), 
-            (select content from sale_order_events_temp where id = i)
-            , '');
-    end loop;
-    RETURN i;
-END;
-$BODY$
-LANGUAGE plpgsql VOLATILE
+group by order_id, order_date, external_id, customer_id) a;
