938838774520e8766b20dbfd8e956e21947aa045
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-03-21 17:48:34 +0530

    Gurpreet| #1965| Ensure that liquibase migrations are copied to /packages before running them

--- scripts/vagrant-database.sh
+++ scripts/vagrant-database.sh
@@ -2,4 +2,7 @@
 PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $PATH_OF_CURRENT_SCRIPT/vagrant/vagrant_functions.sh
 
+run_in_vagrant -c "sudo rm -rf /packages/build/jss_config/migrations/*"
+run_in_vagrant -c "sudo chown jss:jss /packages/build/jss_config/migrations"
+run_in_vagrant -c "sudo su - jss -c 'cp /Project/jss-config/migrations/* /packages/build/jss_config/migrations/'"
 run_in_vagrant -c "sudo su - jss -c 'cd /bahmni_temp/ && ./run-implementation-liquibase.sh'"
