a1519bdb3fff8fe505c2a16b3403ec27930c3318
rohanpoddar <rohan.poddar@gmail.com>
2014-06-20 17:08:21 +0530

    Rohan , Mujir | #248 | Added the migrations for openerp atomfeed scheduler jobs

--- scripts/package.sh
+++ scripts/package.sh
@@ -10,7 +10,7 @@ rm -rf $ROOT_DIR/target/jss_config.zip
 $BASE_DIR/validate-json.sh $ROOT_DIR/openmrs
 if [[ $? -eq 0 ]]
     then
-        cd $ROOT_DIR && zip -r target/jss_config.zip openmrs/* migrations/* openelis/*
+        cd $ROOT_DIR && zip -r target/jss_config.zip openmrs/* migrations/* openelis/* openerp/*
     else
         echo "Validation failed. Please fix the errors and try again"
         exit 1
