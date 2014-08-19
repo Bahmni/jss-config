a8f8ed8bad767b875c2780390a84e62ae3a76934
Mujir <mujirudidn@gmail.com>
2014-08-19 22:22:24 +0530

    Mujir, Mihir | #457 | refactored patient matching algorithm for JSS

--- openmrs/patientMatchingAlgorithm/JSSPatientMatchingAlgorithm.groovy
+++ openmrs/patientMatchingAlgorithm/JSSPatientMatchingAlgorithm.groovy
@@ -3,20 +3,18 @@ import org.bahmni.module.admin.csv.patientmatchingalgorithm.PatientMatchingAlgor
 import org.bahmni.module.admin.csv.patientmatchingalgorithm.exception.CannotMatchPatientException
 import org.openmrs.Patient
 
-
-public class JSSPatientMatchingAlgorithm implements PatientMatchingAlgorithm {
-    private Map<String, String> patientAttributesMap;
-
-
+public class JSSPatientMatchingAlgorithm extends PatientMatchingAlgorithm {
     @Override
     Patient run(List<Patient> patientList, List<KeyValue> patientAttributes) {
-        patientAttributesMap = getPatientAttributes(patientAttributes);
+        String genderFromCSV = valueFor("Gender", patientAttributes);
+        String nameFromCSV = valueFor("Name", patientAttributes);
+
         List<Patient> matchingPatients = new ArrayList<Patient>();
         if (patientList == null || patientList.size() < 1) {
             throw new CannotMatchPatientException();
         }
         for (Patient patient : patientList) {
-            if (fitsCriteria(patient)) {
+            if (matchGenderAndName(patient, genderFromCSV, nameFromCSV)) {
                 matchingPatients.add(patient);
             }
         }
@@ -29,38 +27,14 @@ public class JSSPatientMatchingAlgorithm implements PatientMatchingAlgorithm {
         }
     }
 
-    private boolean fitsCriteria(Patient patient) {
-        if (isGenderSame(patient.getGender())) {
-            if (isNameSame(patient)) {
-                return true;
-            }
-        }
-        return false;
+    private boolean matchGenderAndName(Patient patient, String genderFromCSV, String nameFromCSV) {
+        return patient.getGender().equalsIgnoreCase(genderFromCSV) &&
+                doesAnyNamePartMatch(patient, nameFromCSV);
     }
 
-    private boolean isNameSame(Patient patient) {
-        String uploadedPatientName = patientAttributesMap.get("Name");
-        def givenName = patient.getGivenName();
-        def familyName = patient.getFamilyName();
-        if (uploadedPatientName.contains(givenName) ||
-                uploadedPatientName.contains(familyName)) {
-            return true;
-        }
-        return false;
-    }
-
-    private boolean isGenderSame(String gender) {
-        if (gender.equals(patientAttributesMap.get("Gender"))) {
-            return true;
-        }
-        return false;
-    }
-
-    private Map<String, String> getPatientAttributes(List<KeyValue> patientAttributes) {
-        Map<String, String> patientAttributeMap = new HashMap<String, String>();
-        for (KeyValue patientAttribute : patientAttributes) {
-            patientAttributeMap.put(patientAttribute.getKey(), patientAttribute.getValue());
-        }
-        return patientAttributeMap;
+    private boolean doesAnyNamePartMatch(Patient patient, String uploadedPatientName) {
+        def nameParts = uploadedPatientName.split(" ");
+        return nameParts.any { it.equalsIgnoreCase(patient.getGivenName()) } ||
+               nameParts.any { it.equalsIgnoreCase(patient.getFamilyName()) }
     }
 }
--- scripts/vagrant-link.sh
+++ scripts/vagrant-link.sh
@@ -6,6 +6,8 @@ source $PATH_OF_CURRENT_SCRIPT/vagrant/vagrant_functions.sh
 run_in_vagrant -c "sudo rm -rf /var/www/bahmni_config"
 run_in_vagrant -c "sudo ln -s /Project/jss-config /var/www/bahmni_config"
 run_in_vagrant -c "sudo chown -h jss:jss /var/www/bahmni_config"
+run_in_vagrant -c "sudo ln -s /Project/jss-config/openmrs/patientMatchingAlgorithm /home/jss/.OpenMRS/patientMatchingAlgorithm"
+run_in_vagrant -c "sudo ln -s /Project/jss-config/openmrs/obscalculator /home/jss/.OpenMRS/obscalculator"
 
 
 
