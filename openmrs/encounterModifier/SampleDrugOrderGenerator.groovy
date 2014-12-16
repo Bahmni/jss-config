e9eeddaafccd42633401ed355130617ac776aefc
rohanpoddar <rohan.poddar@gmail.com>
2014-12-16 20:34:44 +0530

    Rohan, Shruthi | #1373 | Modified the groovy script for variable dosing instructions

--- openmrs/encounterModifier/SampleDrugOrderGenerator.groovy
+++ openmrs/encounterModifier/SampleDrugOrderGenerator.groovy
@@ -15,47 +15,51 @@ import org.openmrs.module.emrapi.encounter.domain.EncounterTransaction
 
 public class SampleDrugOrderGenerator extends EncounterModifier {
 
+    public static final String REGIMEN_CONCEPT_NAME = "Breast Cancer Regimen 2"
+    public static final String PERCENTAGE_AMPUTATED_CONCEPT_NAME = "Percentage Amputated"
+    public static final String HEIGHT_CONCEPT_NAME = "Height"
+    public static final String WEIGHT_CONCEPT_NAME = "Weight"
+
     public BahmniEncounterTransaction run(BahmniEncounterTransaction bahmniEncounterTransaction, ConceptData conceptSetData) {
 
         Patient patient = Context.getPatientService().getPatientByUuid(bahmniEncounterTransaction.getPatientUuid());
         def nowAsOfEncounter = bahmniEncounterTransaction.getEncounterDateTime() != null ? bahmniEncounterTransaction.getEncounterDateTime() : new Date();
         def patientAgeInYears = Years.yearsBetween(new LocalDate(patient.getBirthdate()), new LocalDate(nowAsOfEncounter)).getYears();
 
-        def height = fetchLatestValue("Height", bahmniEncounterTransaction.getPatientUuid());
-        def weight = fetchLatestValue("Weight", bahmniEncounterTransaction.getPatientUuid());
+        def height = fetchLatestValue(HEIGHT_CONCEPT_NAME, bahmniEncounterTransaction.getPatientUuid());
+        def weight = fetchLatestValue(WEIGHT_CONCEPT_NAME, bahmniEncounterTransaction.getPatientUuid());
         def bsa = calculateBSA(height, weight, patientAgeInYears);
 
         ObjectMapper objectMapper = new ObjectMapper();
         bahmniEncounterTransaction = objectMapper.convertValue(bahmniEncounterTransaction, new TypeReference<BahmniEncounterTransaction>() {
         });
-
         List<BahmniObservation> bahmniObservations = bahmniEncounterTransaction.getObservations();
 
-        BahmniObservation regimenObservation = findObservation("Breast Cancer Regimen 2", bahmniObservations);
+        BahmniObservation regimenObservation = findObservation(REGIMEN_CONCEPT_NAME, bahmniObservations);
 
         if (regimenObservation == null || regimenObservation.getValue() == null) {
             return bahmniEncounterTransaction;
         }
 
-        BahmniObservation percentageAmputatedObservation = findObservation("Percentage Amputated", bahmniObservations);
+        BahmniObservation percentageAmputatedObservation = findObservation(PERCENTAGE_AMPUTATED_CONCEPT_NAME, bahmniObservations);
 
         List<EncounterTransaction.DrugOrder> drugOrders = bahmniEncounterTransaction.getDrugOrders();
         if ("CAF (6 cycles)".equals(getCodedObsValue(regimenObservation.getValue()))) {
-            drugOrders.add(getDrugOrder("DNS", "Injection", 1000.0, "ml", "Intravenous", "Immediately", null, 2.0, "Unit(s)", 4, "Hour(s)", null));
-            drugOrders.add(getDrugOrder("Dexamethasone 8mg/2ml", "Injection", 2.0, "ml", "Intravenous", "Immediately", null, 1.0, "Unit(s)", 1, "Hour(s)", null));
-            drugOrders.add(getDrugOrder("Ondansetron 8mg/4ml", "Injection", 2.0, "ml", "Intravenous", "Immediately", null, 1.0, "Unit(s)", 1, "Hour(s)", null));
+            drugOrders.add(createUniformDrugOrder("DNS", "Injection", 1000.0, "ml", "Intravenous", "Immediately", 2.0, "Unit(s)", 4, "Hour(s)", null));
+            drugOrders.add(createUniformDrugOrder("Dexamethasone 8mg/2ml", "Injection", 2.0, "ml", "Intravenous", "Immediately", 1.0, "Unit(s)", 1, "Minute(s)", null));
+            drugOrders.add(createUniformDrugOrder("Ondansetron 8mg/4ml", "Injection", 2.0, "ml", "Intravenous", "Immediately", 1.0, "Unit(s)", 1, "Minute(s)", null));
             def cyclophosphamideDose = calculateDose(600.0, bsa, percentageAmputatedObservation);
-            drugOrders.add(getDrugOrder("Cyclophosphamide 500mg", "Injection", cyclophosphamideDose, "ml", "Intravenous", "Immediately", null, Math.round(cyclophosphamideDose), "Unit(s)", 4, "Hour(s)", "with Dextrox 5% (Injection)"));
+            drugOrders.add(createUniformDrugOrder("Cyclophosphamide 500mg", "Injection", cyclophosphamideDose, "mg", "Intravenous", "Immediately", cyclophosphamideDose, "Unit(s)", 4, "Hour(s)", "with Dextrox 5% (Injection)"));
             def doxorubicinHydrochlorideDose = calculateDose(600.0, bsa, percentageAmputatedObservation);
-            drugOrders.add(getDrugOrder("Doxorubicin Hydrochloride 50mg", "Injection", doxorubicinHydrochlorideDose, "ml", "Intravenous", "Immediately", null, Math.round(doxorubicinHydrochlorideDose), "Unit(s)", 4, "Hour(s)", "with Dextrox 5% (Injection)"));
+            drugOrders.add(createUniformDrugOrder("Doxorubicin Hydrochloride 50mg", "Injection", doxorubicinHydrochlorideDose, "mg", "Intravenous", "Immediately", doxorubicinHydrochlorideDose, "Unit(s)", 4, "Hour(s)", "with Dextrox 5% (Injection)"));
             def flurouracilDose = calculateDose(50.0, bsa, percentageAmputatedObservation);
-            drugOrders.add(getDrugOrder("Flurouracil 10ml", "Injection", flurouracilDose, "ml", "Intravenous", "Immediately", null, Math.round(flurouracilDose), "Unit(s)", 4, "Hour(s)", "with Dextrox 5% (Injection)"));
-            drugOrders.add(getDrugOrder("Dextrox 5%", "Injection", 500.0, "ml", "Intravenous", "Immediately", null, 0, "Unit(s)", 1, "Day(s)", "diluent"));
-            drugOrders.add(getDrugOrder("Ondan Setron 4mg", "Tablet", null, "Tablet(s)", "Oral", null, "{\"morningDose\":1,\"afternoonDose\":0,\"eveningDose\":1}", 6, "Tablet(s)", 3, "Day(s)", null));
-            drugOrders.add(getDrugOrder("Chlorpromazine 50mg", "Tablet", null, "Tablet(s)", "Oral", null, "{\"morningDose\":0.5,\"afternoonDose\":0,\"eveningDose\":0.5}", 5, "Tablet(s)", 10, "Day(s)", null));
-            drugOrders.add(getDrugOrder("Tamoxifen 10mg", "Tablet", 2, "Tablet(s)", "Oral", "Once a day", null, 120, "Tablet(s)", 2, "Month(s)", null));
-            drugOrders.add(getDrugOrder("B-Complex", "Tablet", 1, "Tablet(s)", "Oral", "Once a day", null, 30, "Tablet(s)", 1, "Month(s)", null));
-            drugOrders.add(getDrugOrder("Ferrous Sulphate with Folic Acid Large", "Tablet", 1, "Tablet(s)", "Oral", "Once a day", null, 30, "Tablet(s)", 1, "Month(s)", null));
+            drugOrders.add(createUniformDrugOrder("Flurouracil 10ml", "Injection", flurouracilDose, "mg", "Intravenous", "Immediately", flurouracilDose, "Unit(s)", 4, "Hour(s)", "with Dextrox 5% (Injection)"));
+            drugOrders.add(createUniformDrugOrder("Dextrox 5%", "Injection", 500.0, "ml", "Intravenous", "Immediately", 0, "Unit(s)", 1, "Day(s)", "diluent"));
+            drugOrders.add(createVariableDrugOrder("Ondan Setron 4mg", "Tablet", 1, 0, 1, "Tablet(s)", "Oral", null, 6, "Tablet(s)", 3, "Day(s)", null));
+            drugOrders.add(createVariableDrugOrder("Chlorpromazine 50mg", "Tablet", 0.5, 0, 0.5, "Tablet(s)", "Oral", null, 5, "Tablet(s)", 10, "Day(s)", null));
+            drugOrders.add(createUniformDrugOrder("Tamoxifen 10mg", "Tablet", 2, "Tablet(s)", "Oral", "Once a day", 120, "Tablet(s)", 2, "Month(s)", null));
+            drugOrders.add(createUniformDrugOrder("B-Complex", "Tablet", 1, "Tablet(s)", "Oral", "Once a day", 30, "Tablet(s)", 1, "Month(s)", null));
+            drugOrders.add(createUniformDrugOrder("Ferrous Sulphate with Folic Acid Large", "Tablet", 1, "Tablet(s)", "Oral", "Once a day", 30, "Tablet(s)", 1, "Month(s)", null));
         }
         bahmniEncounterTransaction.setDrugOrders(drugOrders);
         return bahmniEncounterTransaction;
@@ -75,7 +79,7 @@ public class SampleDrugOrderGenerator extends EncounterModifier {
 
     static double calculateDose(double referenceDose, double bsa, BahmniObservation percentageAmputatedObservation) {
         double percentageAmputated = percentageAmputatedObservation != null && percentageAmputatedObservation.getValue() != null ? (Double.parseDouble((String)percentageAmputatedObservation.getValue())) : 0;
-        return referenceDose * bsa * (100 - percentageAmputated) / 100;
+        return Math.round(referenceDose * bsa * (100 - percentageAmputated) / 100);
     }
 
     private BahmniObservation findObservation(String conceptName, List<BahmniObservation> bahmniObservations) {
@@ -111,6 +115,7 @@ public class SampleDrugOrderGenerator extends EncounterModifier {
         return null
     }
 
+    //Calculation of BSA by Du Bois Formula: http://www.cato.eu/body-surface-area.html
     static Double calculateBSA(double height, double weight, int patientAgeInYears) {
         if (patientAgeInYears <= 15 && weight <= 40) {
             return Math.sqrt(weight * height / 3600);
@@ -119,11 +124,38 @@ public class SampleDrugOrderGenerator extends EncounterModifier {
     }
 
     private
-    static EncounterTransaction.DrugOrder getDrugOrder(String drugName, String drugForm, Double dose, String doseUnits,
-                                                       String route, String frequency,
-                                                       String administrationInstructions, Double quantity,
-                                                       String quantityUnit, Integer duration, String durationUnits,
-                                                       String additionalInstructions) {
+    static EncounterTransaction.DrugOrder createUniformDrugOrder(String drugName, String drugForm, Double dose,
+                                                                 String doseUnits, String route, String frequency, Double quantity,
+                                                                 String quantityUnit, Integer duration, String durationUnits, String additionalInstructions) {
+        def drugOrder = createDrugOrder(drugName, drugForm, doseUnits, route, frequency, quantity, quantityUnit, duration, durationUnits, additionalInstructions)
+        drugOrder.dosingInstructions.dose = dose;
+        if(additionalInstructions != null){
+            def instructions = ["additionalInstructions" : additionalInstructions]
+            ObjectMapper om = new ObjectMapper();
+            def administrationInstructions = om.writeValueAsString(instructions);
+            drugOrder.dosingInstructions.setAdministrationInstructions(administrationInstructions);
+        }
+        return drugOrder;
+    }
+
+
+    private
+    static EncounterTransaction.DrugOrder createVariableDrugOrder(String drugName, String drugForm, Double morningDose, Double afternoonDose, Double eveningDose,
+                                                                  String doseUnits, String route, String frequency, Double quantity, String quantityUnit,
+                                                                  Integer duration, String durationUnits, String additionalInstructions) {
+        def drugOrder = createDrugOrder(drugName, drugForm, doseUnits, route, frequency, quantity, quantityUnit, duration, durationUnits, additionalInstructions)
+        def doses = ["morningDose" : morningDose, "afternoonDose" : afternoonDose, "eveningDose" : eveningDose];
+        if(additionalInstructions != null){
+            doses.put("additionalInstructions", additionalInstructions);
+        }
+        ObjectMapper om = new ObjectMapper();
+        def administrationInstructions = om.writeValueAsString(doses);
+        drugOrder.dosingInstructions.setAdministrationInstructions(administrationInstructions);
+        return drugOrder;
+    }
+
+    private
+    static EncounterTransaction.DrugOrder createDrugOrder(String drugName, String drugForm, String doseUnits, String route, String frequency, double quantity, String quantityUnit, int duration, String durationUnits, String additionalInstructions) {
         EncounterTransaction.DrugOrder drugOrder = new EncounterTransaction.DrugOrder();
         EncounterTransaction.Drug drug = new EncounterTransaction.Drug();
         drug.setName(drugName);
@@ -134,11 +166,9 @@ public class SampleDrugOrderGenerator extends EncounterModifier {
         drugOrder.setDosingInstructionType("org.openmrs.module.bahmniemrapi.drugorder.dosinginstructions.FlexibleDosingInstructions");
 
         EncounterTransaction.DosingInstructions dosingInstructions = new EncounterTransaction.DosingInstructions();
-        dosingInstructions.setDose(dose);
         dosingInstructions.setDoseUnits(doseUnits);
         dosingInstructions.setRoute(route);
         dosingInstructions.setFrequency(frequency);
-        dosingInstructions.setAdministrationInstructions(administrationInstructions);
         dosingInstructions.setQuantity(quantity);
         dosingInstructions.setQuantityUnits(quantityUnit);
         dosingInstructions.setAsNeeded(false);
@@ -146,8 +176,6 @@ public class SampleDrugOrderGenerator extends EncounterModifier {
 
         drugOrder.setDuration(duration);
         drugOrder.setDurationUnits(durationUnits);
-
-        drugOrder.setInstructions(additionalInstructions);
 
         return drugOrder;
     }
