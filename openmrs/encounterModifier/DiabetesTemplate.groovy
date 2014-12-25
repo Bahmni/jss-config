1f33872958b523dcbe309d4c5a24a4a3e5c1a318
Vivek Singh <petmongrels@gmail.com>
2014-12-25 12:52:03 +0530

    removed patient profile, merged my other changes

--- openmrs/encounterModifier/DiabetesTemplate.groovy
+++ openmrs/encounterModifier/DiabetesTemplate.groovy
@@ -1,67 +1,99 @@
 import org.bahmni.module.bahmnicore.contract.encounter.data.ConceptData
 import org.bahmni.module.bahmnicore.encounterModifier.EncounterModifier
 import org.codehaus.jackson.map.ObjectMapper
-import org.hibernate.Query
-import org.hibernate.SessionFactory
-import org.openmrs.Obs
+import org.openmrs.Drug
 import org.openmrs.Patient
 import org.openmrs.api.context.Context
 import org.openmrs.module.bahmniemrapi.encountertransaction.contract.BahmniEncounterTransaction
-import org.openmrs.module.bahmniemrapi.encountertransaction.contract.BahmniObservation
+import org.openmrs.module.emrapi.diagnosis.Diagnosis
+import org.openmrs.module.emrapi.diagnosis.DiagnosisService
 import org.openmrs.module.emrapi.encounter.domain.EncounterTransaction
 
-public class CancerRegimen extends EncounterModifier {
+public class DiabetesTemplate extends EncounterModifier {
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    static private String TYPE1 = 'Diabetes Mellitus, Type 1'
+    static private String TYPE2 = 'Diabetes Mellitus, Type 2'
+    static private String UNSPECIFIED = 'Diabetes Mellitus, Unspecified'
+    static private String KETOACIDOSIS = 'Diabetes, Ketoacidosis'
+    static private String GESTATIONAL = 'Diabetes, Gestational'
+    static private diabetesDiagnosis = [TYPE1, TYPE2, UNSPECIFIED, KETOACIDOSIS, GESTATIONAL]
+
+    static private String INSULIN_30_70_CARTIGES = "Insulin 30/70 Cartiges"
+    static private String BIPHASIC_ISOPHANE_INSULIN_30_70 = "Biphasic Isophane Insulin 30/70"
+    static private String BIPHASIC_ISOPHANE_INSULIN_50_50 = "Biphasic Isophane Insulin 50/50"
+    static private String INSULIN_PLAIN = "Insulin, plain"
+    static private String PIOGLITAZONE = "Pioglitazone 15mg"
+    static private String GLIPIZIDE = "Glipizide 5mg"
+    static private String METFORMIN = "Metformin 500mg (SR)"
+    static private String GLIBENCLAMIDE = "Glibenclamide 5mg"
+    static private String GLIMEPIRIDE = "Glimepiride"
 
     public BahmniEncounterTransaction run(BahmniEncounterTransaction bahmniEncounterTransaction, ConceptData conceptSetData) {
-        Patient patient = Context.getPatientService().getPatientByUuid(bahmniEncounterTransaction.getPatientUuid());
+        Patient patient = Context.getPatientService().getPatientByUuid(bahmniEncounterTransaction.getPatientUuid())
+        def diabetesType = whichDiabetes(patient, bahmniEncounterTransaction)
 
         List<EncounterTransaction.DrugOrder> drugOrders = bahmniEncounterTransaction.getDrugOrders();
-
-        drugOrders.add(createUniformDrugOrder("DNS", "Injection", 1000.0, "ml", "Intravenous", "Immediately", 2.0, "Unit(s)", 4, "Hour(s)", null));
-        drugOrders.add(createUniformDrugOrder("Dexamethasone 8mg/2ml", "Injection", 2.0, "ml", "Intravenous", "Immediately", 1.0, "Unit(s)", 1, "Minute(s)", null));
-        drugOrders.add(createUniformDrugOrder("Ondansetron 8mg/4ml", "Injection", 2.0, "ml", "Intravenous", "Immediately", 1.0, "Unit(s)", 1, "Minute(s)", null));
-        def cyclophosphamideDose = calculateDose(600.0, bsa, percentageAmputatedObservation);
-        drugOrders.add(createUniformDrugOrder("Cyclophosphamide 500mg", "Injection", cyclophosphamideDose, "mg", "Intravenous", "Immediately", cyclophosphamideDose, "Unit(s)", 4, "Hour(s)", "with Dextrox 5% (Injection)"));
-        def doxorubicinHydrochlorideDose = calculateDose(600.0, bsa, percentageAmputatedObservation);
-        drugOrders.add(createUniformDrugOrder("Doxorubicin Hydrochloride 50mg", "Injection", doxorubicinHydrochlorideDose, "mg", "Intravenous", "Immediately", doxorubicinHydrochlorideDose, "Unit(s)", 4, "Hour(s)", "with Dextrox 5% (Injection)"));
-        def flurouracilDose = calculateDose(50.0, bsa, percentageAmputatedObservation);
-        drugOrders.add(createUniformDrugOrder("Flurouracil 10ml", "Injection", flurouracilDose, "mg", "Intravenous", "Immediately", flurouracilDose, "Unit(s)", 4, "Hour(s)", "with Dextrox 5% (Injection)"));
-        drugOrders.add(createUniformDrugOrder("Dextrox 5%", "Injection", 500.0, "ml", "Intravenous", "Immediately", 0, "Unit(s)", 1, "Day(s)", "diluent"));
-        drugOrders.add(createVariableDrugOrder("Ondan Setron 4mg", "Tablet", 1, 0, 1, "Tablet(s)", "Oral", null, 6, "Tablet(s)", 3, "Day(s)", null));
-        drugOrders.add(createVariableDrugOrder("Chlorpromazine 50mg", "Tablet", 0.5, 0, 0.5, "Tablet(s)", "Oral", null, 5, "Tablet(s)", 10, "Day(s)", null));
-        drugOrders.add(createUniformDrugOrder("Tamoxifen 10mg", "Tablet", 2, "Tablet(s)", "Oral", "Once a day", 120, "Tablet(s)", 2, "Month(s)", null));
-        drugOrders.add(createUniformDrugOrder("B-Complex", "Tablet", 1, "Tablet(s)", "Oral", "Once a day", 30, "Tablet(s)", 1, "Month(s)", null));
-        drugOrders.add(createUniformDrugOrder("Ferrous Sulphate with Folic Acid Large", "Tablet", 1, "Tablet(s)", "Oral", "Once a day", 30, "Tablet(s)", 1, "Month(s)", null));
-
-        bahmniEncounterTransaction.setDrugOrders(drugOrders);
-
-        return bahmniEncounterTransaction;
-    }
-
-    static Double fetchDiabetesDiagnosis(String patientUuid) {
-        SessionFactory sessionFactory = Context.getRegisteredComponents(SessionFactory.class).get(0)
-        Query queryToGetObservations = sessionFactory.getCurrentSession()
-                .createQuery("select obs " +
-                " from Obs as obs, ConceptName as cn " +
-                " where obs.person.uuid = :patientUuid " +
-                " and cn.concept = obs.concept.conceptId " +
-                " and cn.name = :conceptName " +
-                " and obs.voided = false" +
-                " order by obs.obsDatetime desc limit 1");
-        queryToGetObservations.setString("patientUuid", patientUuid);
-        queryToGetObservations.setParameterList("conceptName", conceptName);
-        List<Obs> observations = queryToGetObservations.list();
-        if (observations.size() > 0) {
-            return observations.get(0).getValueNumeric();
+        switch (diabetesType) {
+            case TYPE1:
+                drugOrders.addAll(
+                        drugOrder(INSULIN_30_70_CARTIGES, ROUTE.SUBCUTANEOUS),
+                        drugOrder(BIPHASIC_ISOPHANE_INSULIN_30_70, ROUTE.SUBCUTANEOUS),
+                        drugOrder(BIPHASIC_ISOPHANE_INSULIN_50_50, ROUTE.SUBCUTANEOUS),
+//                        drugOrder(INSULIN_PLAIN, ROUTE.SUBCUTANEOUS)
+                )
+                break
+            case TYPE2:
+                drugOrders.addAll(
+                        drugOrder(GLIPIZIDE, 1, DOSAGE.TABLETS, ROUTE.ORAL, DOSAGE_FREQUENCY.ONCE_A_DAY, 30, DOSAGE.TABLETS, 1, DURATION.MONTHS, "Before breakfast"),
+                        drugOrder(METFORMIN, 1, DOSAGE.TABLETS, ROUTE.ORAL, DOSAGE_FREQUENCY.ONCE_A_DAY, 30, DOSAGE.TABLETS, 1, DURATION.MONTHS, "After lunch"),
+                        drugOrder(PIOGLITAZONE, 1, DOSAGE.TABLETS, ROUTE.ORAL, DOSAGE_FREQUENCY.ONCE_A_DAY, 30, DOSAGE.TABLETS, 1, DURATION.MONTHS, "After lunch"),
+                        drugOrder(INSULIN_30_70_CARTIGES, null, DOSAGE.IU, ROUTE.SUBCUTANEOUS, DOSAGE_FREQUENCY.TWICE_A_DAY, null, "Unit(s)", 1, DURATION.MONTHS, "Before breakfast and before dinner"),
+                        drugOrder(BIPHASIC_ISOPHANE_INSULIN_30_70, null, DOSAGE.IU, ROUTE.SUBCUTANEOUS, DOSAGE_FREQUENCY.TWICE_A_DAY, null, "Unit(s)", 1, DURATION.MONTHS, "Before breakfast and before dinner"),
+                )
+                break
+            case KETOACIDOSIS:
+                drugOrders.addAll(
+//                        drugOrder(INSULIN_PLAIN, null, DOSAGE.IU, ROUTE.SUBCUTANEOUS, DOSAGE_FREQUENCY.THRICE_A_DAY, null, "Unit(s)", null, null, "Before meals"),
+                        drugOrder(BIPHASIC_ISOPHANE_INSULIN_50_50, null, DOSAGE.IU, ROUTE.SUBCUTANEOUS, DOSAGE_FREQUENCY.TWICE_A_DAY, null, "Unit(s)", 1, DURATION.MONTHS, null),
+                        drugOrder(GLIBENCLAMIDE, 1, DOSAGE.TABLETS, ROUTE.ORAL, DOSAGE_FREQUENCY.ONCE_A_DAY, 30, DOSAGE.TABLETS, 1, DURATION.MONTHS, null),
+//                        drugOrder(GLIMEPIRIDE, 1, DOSAGE.TABLETS, ROUTE.ORAL, DOSAGE_FREQUENCY.ONCE_A_DAY, 30, DOSAGE.TABLETS, 1, DURATION.MONTHS, "With breakfast or lunch"),
+                )
+                break
+            case GESTATIONAL:
+                drugOrders.addAll(
+                        drugOrder(INSULIN_30_70_CARTIGES, ROUTE.SUBCUTANEOUS),
+                        drugOrder(BIPHASIC_ISOPHANE_INSULIN_30_70, ROUTE.SUBCUTANEOUS),
+                        drugOrder(BIPHASIC_ISOPHANE_INSULIN_50_50, ROUTE.SUBCUTANEOUS),
+                        drugOrder(PIOGLITAZONE, ROUTE.ORAL))
+                break
         }
-        return null
+        bahmniEncounterTransaction.setDrugOrders(drugOrders)
+
+        return bahmniEncounterTransaction
     }
 
-    private static EncounterTransaction.DrugOrder createUniformDrugOrder(String drugName, String drugForm, Double dose,
-                                                                 String doseUnits, String route, String frequency, Double quantity,
-                                                                 String quantityUnit, Integer duration, String durationUnits, String additionalInstructions) {
-        def drugOrder = createDrugOrder(drugName, drugForm, doseUnits, route, frequency, quantity, quantityUnit, duration, durationUnits, additionalInstructions)
+    static String form(String name) {
+        Drug drug = Context.getConceptService().getDrug(name)
+        if (drug == null) throw new NullPointerException("Drug with name: $name doesn't exist")
+        return drug.getDosageForm().getName().getName()
+    }
+
+    static String whichDiabetes(Patient patient, BahmniEncounterTransaction bahmniEncounterTransaction) {
+        DiagnosisService diagnosisService = Context.getService(DiagnosisService.class)
+        List<Diagnosis> diagnoses = new ArrayList<Diagnosis>(diagnosisService.getUniqueDiagnoses(patient, null))
+        Collections.sort(diagnoses, new DiagnosisComparator())
+        if (diagnoses.size() == 0) return ""
+        return diagnoses.last().getDiagnosis().getCodedAnswer().getName().getName()
+    }
+
+    private static EncounterTransaction.DrugOrder drugOrder(String drugName, String route) {
+        return drugOrder(drugName, null, null, route, null, null, null, null, null, null);
+    }
+
+    private static EncounterTransaction.DrugOrder drugOrder(String drugName, Double dose,
+                                                            String doseUnits, String route, String frequency, Double quantity,
+                                                            String quantityUnit, Integer duration, String durationUnits, String additionalInstructions) {
+        def drugOrder = createDrugOrder(drugName, form(drugName), doseUnits, route, frequency, quantity, quantityUnit, duration, durationUnits)
         drugOrder.dosingInstructions.dose = dose;
         if (additionalInstructions != null) {
             def instructions = ["additionalInstructions": additionalInstructions]
@@ -71,26 +103,13 @@ public class CancerRegimen extends EncounterModifier {
         return drugOrder;
     }
 
-
-    private
-    static EncounterTransaction.DrugOrder createVariableDrugOrder(String drugName, String drugForm, Double morningDose, Double afternoonDose, Double eveningDose,
-                                                                  String doseUnits, String route, String frequency, Double quantity, String quantityUnit,
-                                                                  Integer duration, String durationUnits, String additionalInstructions) {
-        def drugOrder = createDrugOrder(drugName, drugForm, doseUnits, route, frequency, quantity, quantityUnit, duration, durationUnits, additionalInstructions)
-        def doses = ["morningDose": morningDose, "afternoonDose": afternoonDose, "eveningDose": eveningDose];
-        if (additionalInstructions != null) {
-            doses.put("additionalInstructions", additionalInstructions);
-        }
-        def administrationInstructions = convertToJSON(doses);
-        drugOrder.dosingInstructions.setAdministrationInstructions(administrationInstructions);
-        return drugOrder;
-    }
-
     private static String convertToJSON(LinkedHashMap<String, Double> doses) {
         objectMapper.writeValueAsString(doses)
     }
 
-    private static EncounterTransaction.DrugOrder createDrugOrder(String drugName, String drugForm, String doseUnits, String route, String frequency, double quantity, String quantityUnit, int duration, String durationUnits, String additionalInstructions) {
+    private
+    static EncounterTransaction.DrugOrder createDrugOrder(String drugName, String drugForm, String doseUnits, String route, String frequency,
+                                                          Double quantity, String quantityUnit, Integer duration, String durationUnits) {
         EncounterTransaction.DrugOrder drugOrder = new EncounterTransaction.DrugOrder();
         EncounterTransaction.Drug drug = new EncounterTransaction.Drug();
         drug.setName(drugName);
@@ -104,14 +123,45 @@ public class CancerRegimen extends EncounterModifier {
         dosingInstructions.setDoseUnits(doseUnits);
         dosingInstructions.setRoute(route);
         dosingInstructions.setFrequency(frequency);
-        dosingInstructions.setQuantity(quantity);
+        if (quantity != null)
+            dosingInstructions.setQuantity(quantity);
         dosingInstructions.setQuantityUnits(quantityUnit);
         dosingInstructions.setAsNeeded(false);
         drugOrder.setDosingInstructions(dosingInstructions);
 
-        drugOrder.setDuration(duration);
+        if (quantity != null)
+            drugOrder.setDuration(duration);
         drugOrder.setDurationUnits(durationUnits);
 
         return drugOrder;
+    }
+
+    static class ROUTE {
+        public static String SUBCUTANEOUS = "Sub Cutaneous"
+        public static String ORAL = "Oral"
+    }
+
+    static class DOSAGE {
+        public static String TABLETS = "Tablet(s)"
+        public static String IU = "IU"
+    }
+
+    static class DOSAGE_FREQUENCY {
+        public static String ONCE_A_DAY = "Once a day"
+        public static String TWICE_A_DAY = "Twice a day"
+        public static String THRICE_A_DAY = "Thrice a day"
+    }
+
+    static class DURATION {
+        public static String MONTHS = "Month(s)"
+    }
+
+    static class DiagnosisComparator implements Comparator<Diagnosis> {
+        @Override
+        int compare(Diagnosis o1, Diagnosis o2) {
+            Date date1 = o1.getExistingObs().getObsDatetime()
+            Date date2 = o2.getExistingObs().getObsDatetime()
+            return date1.compareTo(date2)
+        }
     }
 }
