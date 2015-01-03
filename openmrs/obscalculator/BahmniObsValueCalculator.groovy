5676f41f944ed3420b80bb050436d5bde7750339
Vivek Singh <petmongrels@gmail.com>
2015-01-03 16:15:46 +0530

    voidObs signature fix
    Conflicts:
    	openmrs/obscalculator/BahmniObsValueCalculator.groovy

--- openmrs/obscalculator/BahmniObsValueCalculator.groovy
+++ openmrs/obscalculator/BahmniObsValueCalculator.groovy
@@ -81,7 +81,8 @@ public class BahmniObsValueCalculator implements ObsValueCalculator {
             Date obsDatetime = getDate(weightObservation) != null ? getDate(weightObservation) : getDate(heightObservation)
 
             if (height == null || weight == null) {
-                voidBmiObs(bmiObservation, bmiStatusObservation)
+                voidObs(bmiObservation)
+                voidObs(bmiStatusObservation)
                 return
             }
 
