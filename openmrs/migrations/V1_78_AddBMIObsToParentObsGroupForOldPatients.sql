9d21bafb0d0dbbccedcba7f6ce508db98fdd1580
Vinkesh Banka <vinkesh.banka@gmail.com>
2015-05-29 16:33:03 +0530

    Banka | #1529 | Adding BMI & BMI Status to BMI Data obs group when BMI & BMI Status were not part of any other obs_group.

--- openmrs/migrations/V1_78_AddBMIObsToParentObsGroupForOldPatients.sql
+++ openmrs/migrations/V1_78_AddBMIObsToParentObsGroupForOldPatients.sql
@@ -20,15 +20,25 @@ CREATE PROCEDURE add_bmi_obs_to_parent_obs_group (parent_concept_name VARCHAR(25
     DECLARE abnormal_obs_coded_value INT DEFAULT false_concept_id;
     DECLARE parent_obs_id INT;
     
-    DECLARE obs_cursor CURSOR FOR SELECT obs.obs_id, bmi_status.value_text, obs.encounter_id, obs.person_id, obs.order_id, obs.obs_datetime, obs.creator, obs.date_created
-                                 FROM obs obs
-                                   INNER JOIN obs parent on obs.obs_group_id = parent.obs_id
-                                   LEFT OUTER JOIN obs bmi_status on obs.obs_group_id = bmi_status.obs_group_id and bmi_status.concept_id = bmi_status_concept_id
-                                 WHERE
-                                   obs.voided = false
-                                   AND obs.concept_id = obs_concept_id
-                                   AND obs.obs_group_id IS NOT NULL
-                                   AND parent.concept_id != parent_concept_id;
+    DECLARE obs_cursor CURSOR FOR (SELECT obs.obs_id, bmi_status.value_text, obs.encounter_id, obs.person_id, obs.order_id, obs.obs_datetime, obs.creator, obs.date_created
+                                FROM obs obs
+                                  INNER JOIN obs parent on obs.obs_group_id = parent.obs_id
+                                  LEFT OUTER JOIN obs bmi_status on obs.obs_group_id = bmi_status.obs_group_id and bmi_status.concept_id = bmi_status_concept_id and bmi_status.voided = false
+                                WHERE
+                                  obs.voided = false
+                                  AND obs.concept_id = obs_concept_id
+                                  AND obs.obs_group_id IS NOT NULL
+                                  AND parent.concept_id != parent_concept_id)
+                                UNION
+                                (SELECT obs.obs_id, bmi_status.value_text, obs.encounter_id, obs.person_id, obs.order_id, obs.obs_datetime, obs.creator, obs.date_created
+                                FROM obs obs
+                                  INNER join encounter on obs.encounter_id = encounter.encounter_id and encounter.voided = false
+                                  LEFT OUTER JOIN obs bmi_status on bmi_status.encounter_id = obs.encounter_id and bmi_status.concept_id = bmi_status_concept_id and bmi_status.voided = false
+                                WHERE
+                                  obs.voided = false
+                                  AND obs.concept_id = obs_concept_id
+                                  AND obs.obs_group_id is NULL
+                                );
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
     OPEN obs_cursor;
 
