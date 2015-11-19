158805ad404f4b9fcdc692b3adcbd76408bbf3f5
sadiag <sadia16.ghazal@gmail.com>
2015-11-19 15:32:24 +0530

    sadia | Added Custom Display Control for Death Certificate

--- openmrs/apps/customDisplayControl/js/customControl.js
+++ openmrs/apps/customDisplayControl/js/customControl.js
@@ -8,6 +8,7 @@ angular.module('bahmni.common.displaycontrol.custom')
             spinner.forPromise(observationsService.fetch($scope.patient.uuid, conceptNames, "latest", undefined, $scope.visitUuid, undefined).then(function (response) {
                 $scope.observations = response.data;
             }));
+            
         };
 
         return {
@@ -15,4 +16,34 @@ angular.module('bahmni.common.displaycontrol.custom')
             template: '<ng-include src="contentUrl"/>',
             link: link
         }
-    }])
+    }]).directive('deathCertificate', ['$q','observationsService','visitService', 'bedService','appService', 'spinner','$sce', function ($q,observationsService, visitService, bedService,appService, spinner, $sce) 
+    {
+        var link = function ($scope) 
+        {
+            
+            var conceptNames = ["Death Note"];
+            spinner.forPromise(observationsService.fetch($scope.patient.uuid, conceptNames, "latest", undefined, $scope.visitUuid, undefined).then(function (response) {
+                    $scope.observations = response.data;
+                    
+
+                }));
+            $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/deathCertificate.html";
+            
+            spinner.forPromise($q.all([bedService.getAssignedBedForPatient($scope.patient.uuid),visitService.getVisitSummary($scope.visitUuid)]).then(function(results){
+                    $scope.bedDetails = results[0];
+                    $scope.visitSummary = results[1].data;
+                }));
+                
+        };
+        var controller = function($scope){
+        	$scope.htmlLabel = function(label){
+        		return $sce.trustAsHtml(label)
+        	}
+        }
+        return {
+            restrict: 'E',
+            link: link,
+            controller : controller,
+            template: '<ng-include src="contentUrl"/>'
+        }
+    }]);
