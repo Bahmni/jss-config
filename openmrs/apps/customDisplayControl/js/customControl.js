546bdba266c56bf5167f08ed4dda32c34f68aa11
sandye <sandeepelambassery@gmail.com>
2015-11-20 14:10:52 +0530

    Corrected Splling

--- openmrs/apps/customDisplayControl/js/customControl.js
+++ openmrs/apps/customDisplayControl/js/customControl.js
@@ -4,7 +4,7 @@ angular.module('bahmni.common.displaycontrol.custom')
     .directive('birthCertificate', ['observationsService', 'appService', 'spinner', function (observationsService, appService, spinner) {
         var link = function ($scope) {
             var conceptNames = ["HEIGHT"];
-            $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/birthCertificates.html";
+            $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/birthCertificate.html";
             spinner.forPromise(observationsService.fetch($scope.patient.uuid, conceptNames, "latest", undefined, $scope.visitUuid, undefined).then(function (response) {
                 $scope.observations = response.data;
             }));
