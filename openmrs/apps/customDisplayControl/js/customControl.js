dd197707028457e06b5670fca0edf90852d932ae
Arjun <arjunk@thoughtworks.com>
2016-01-16 22:35:30 +0530

    Arjun : fixing the sequence dependency in CDST forms. also making a few fields as mandatory

--- openmrs/apps/customDisplayControl/js/customControl.js
+++ openmrs/apps/customDisplayControl/js/customControl.js
@@ -52,8 +52,30 @@ angular.module('bahmni.common.displaycontrol.custom')
             
             var conceptNames = ["RNTCP Form"];
             spinner.forPromise(observationsService.fetch($scope.patient.uuid, conceptNames, "latest", undefined, $scope.visitUuid, undefined).then(function (response) {
-                    $scope.observations = response.data[0];
-                    
+                    $scope.observations = response.data[0];   
+                    $scope.rntcpform = []
+                    function createForm(obs) {
+                           if (obs.groupMembers.length == 0){
+                              if ($scope.rntcpform[obs.conceptNameToDisplay] == undefined){
+                                 $scope.rntcpform[obs.conceptNameToDisplay] = obs.valueAsString;
+                              }
+                              else{
+                                 $scope.rntcpform[obs.conceptNameToDisplay] = $scope.rntcpform[obs.conceptNameToDisplay] + ' ' + obs.valueAsString;                                
+                              }
+
+                              if(obs.comment != null){
+                                $scope.rntcpform[obs.conceptNameToDisplay] = $scope.rntcpform[obs.conceptNameToDisplay] + ' ' + obs.comment;
+                              }   
+                           }
+                           else{
+                              for(var i = 0; i < obs.groupMembers.length; i++) { 
+                                   createForm(obs.groupMembers[i]);
+                              }
+                              
+                           }
+
+                    }
+                    createForm(response.data[0]);
 
                 }));
             $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/rntcpform.html";
@@ -83,7 +105,29 @@ angular.module('bahmni.common.displaycontrol.custom')
             var conceptNames = ["Clinincal Information TB"];
             spinner.forPromise(observationsService.fetch($scope.patient.uuid, conceptNames, "latest", undefined, $scope.visitUuid, undefined).then(function (response) {
                     $scope.observations = response.data[0];
-                    
+                    $scope.rmrctForm = [];
+                    function createForm(obs) {
+                           if (obs.groupMembers.length == 0){
+                              if ($scope.rmrctForm[obs.conceptNameToDisplay] == undefined){
+                                 $scope.rmrctForm[obs.conceptNameToDisplay] = obs.valueAsString;
+                              }
+                              else{
+                                 $scope.rmrctForm[obs.conceptNameToDisplay] = $scope.rmrctForm[obs.conceptNameToDisplay] + ' ' + obs.valueAsString;                                
+                              }
+
+                              if(obs.comment != null){
+                                $scope.rmrctForm[obs.conceptNameToDisplay] = $scope.rmrctForm[obs.conceptNameToDisplay] + ' ' + obs.comment;
+                              }   
+                           }
+                           else{
+                              for(var i = 0; i < obs.groupMembers.length; i++) { 
+                                   createForm(obs.groupMembers[i]);
+                              }
+                              
+                           }
+
+                    }
+                    createForm(response.data[0]);
 
                 }));
             $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/clinicalinfotb.html";
@@ -113,6 +157,30 @@ angular.module('bahmni.common.displaycontrol.custom')
             var conceptNames = ["People Health Support Group Form"];
             spinner.forPromise(observationsService.fetch($scope.patient.uuid, conceptNames, "latest", undefined, $scope.visitUuid, undefined).then(function (response) {
                     $scope.observations = response.data[0];
+                    $scope.hindujaform = []
+                    function createForm(obs) {
+                           if (obs.groupMembers.length == 0){
+                              if ($scope.hindujaform[obs.conceptNameToDisplay] == undefined){
+                                 $scope.hindujaform[obs.conceptNameToDisplay] = obs.valueAsString;
+                              }
+                              else{
+                                 $scope.hindujaform[obs.conceptNameToDisplay] = $scope.hindujaform[obs.conceptNameToDisplay] + ' ' + obs.valueAsString;                                
+                              }
+
+                              if(obs.comment != null){
+                                $scope.hindujaform[obs.conceptNameToDisplay] = $scope.hindujaform[obs.conceptNameToDisplay] + ' ' + obs.comment;
+                              }   
+                           }
+                           else{
+                              for(var i = 0; i < obs.groupMembers.length; i++) { 
+                                   createForm(obs.groupMembers[i]);
+                              }
+                              
+                           }
+
+                    }
+                    createForm(response.data[0]);
+
                     
 
                 }));
