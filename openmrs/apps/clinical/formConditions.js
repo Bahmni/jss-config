e418920e885875c022c79a29df1d3549587406f9
Arjun <arjunk@thoughtworks.com>
2016-01-18 10:24:51 +0530

    Arjun : Naming the concepts appropriately

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -4,11 +4,11 @@ Bahmni.ConceptSet.FormConditions.rules = {
 		enable : [],
 		disable : []
 	};
-	var previousTreatmentWhere = "If treatment taken, Where was the patient treated ?";
-	var previousTreamentWhen = "If treatment taken, When was the patient treated ?";
-	var previousTreatmentCount = "If treatment taken, How many times was the patient treated ?";
-	var previousTreatmentDots = "If treatment taken, If Dots taken : Category";
-	var previousTreatmentDrugs = "People Health Support Group Form, Drugs used in his treatment";
+	var previousTreatmentWhere = "RMRCT, If treatment taken, Where was the patient treated ?";
+	var previousTreamentWhen = "RMRCT, If treatment taken, When was the patient treated ?";
+	var previousTreatmentCount = "RMRCT, If treatment taken, How many times was the patient treated ?";
+	var previousTreatmentDots = "RMRCT, If treatment taken, If Dots taken : Category";
+	var previousTreatmentDrugs = "CDST Form, Drugs used in his treatment";
 	var result = formFieldValues['RMRCT, Previsiosly treated with TB'];
 	if (result == "Yes") {
 		conditions.enable.push(previousTreatmentWhere,previousTreamentWhen,previousTreatmentCount,previousTreatmentDots,previousTreatmentDrugs);
