a4583f162026bc94b3f1e09361726e43d7366b72
sadiag <sadia16.ghazal@gmail.com>
2016-05-26 15:36:07 +0530

    sadia | cconfig changes and Creatinine clearance rate

--- openmrs/elisFeedInterceptor/creatinineClearance.groovy
+++ openmrs/elisFeedInterceptor/creatinineClearance.groovy
@@ -71,9 +71,9 @@ public class CreatinineUpdate implements ElisFeedInterceptor {
         double CreatinineClearanceRate = 0.0;
 
         if (gender.equals('M')) {
-            CreatinineClearanceRate = (((140 - personage) * weighttval.getValueNumeric()) / (72 * (obs.getValueNumeric())));
+            CreatinineClearanceRate = math.round(((140 - personage) * weighttval.getValueNumeric()) / (72 * (obs.getValueNumeric())));
         } else if (gender.equals('F')) {
-            CreatinineClearanceRate = (((140 - personage) * weighttval.getValueNumeric()) / (72 * (obs.getValueNumeric()))) * 0.85;
+            CreatinineClearanceRate = math.round(((140 - personage) * weighttval.getValueNumeric()) / (72 * (obs.getValueNumeric()))) * 0.85;
         }
 
 
